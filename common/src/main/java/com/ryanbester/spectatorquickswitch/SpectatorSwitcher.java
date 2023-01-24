b85099005a11add5de453502a29d1ea810e78f4e
Ryan Bester <ryan.bester5@gmail.com>
2023-01-24 19:51:06 +0000

    Add support for 1.19.3

--- common/src/main/java/com/ryanbester/spectatorquickswitch/SpectatorSwitcher.java
+++ common/src/main/java/com/ryanbester/spectatorquickswitch/SpectatorSwitcher.java
@@ -3,7 +3,7 @@ package com.ryanbester.spectatorquickswitch;
 import com.google.common.base.MoreObjects;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.GameType;
 
 public class SpectatorSwitcher {
@@ -11,13 +11,13 @@ public class SpectatorSwitcher {
         LocalPlayer player = Minecraft.getInstance().player;
         if (player != null) {
             if (!player.hasPermissions(2)) {
-                player.displayClientMessage(new TranslatableComponent("debug.creative_spectator.error"), false);
+                player.displayClientMessage(Component.translatable("debug.creative_spectator.error"), false);
             } else if (!player.isSpectator()) {
-                player.chat("/gamemode spectator");
+                player.connection.sendUnsignedCommand("gamemode spectator");
             } else {
                 if (Minecraft.getInstance().gameMode != null) {
                     GameType gameMode = Minecraft.getInstance().gameMode.getPreviousPlayerMode();
-                    player.chat("/gamemode " + MoreObjects.firstNonNull(gameMode, GameType.CREATIVE).getName());
+                    player.connection.sendUnsignedCommand("gamemode " + MoreObjects.firstNonNull(gameMode, GameType.CREATIVE).getName());
                 }
             }
         }
--- forge/src/main/java/com/ryanbester/spectatorquickswitch/forge/SpectatorQuickSwitch.java
+++ forge/src/main/java/com/ryanbester/spectatorquickswitch/forge/SpectatorQuickSwitch.java
@@ -2,36 +2,36 @@ package com.ryanbester.spectatorquickswitch.forge;
 
 import com.ryanbester.spectatorquickswitch.SpectatorSwitcher;
 import net.minecraft.client.KeyMapping;
-import net.minecraftforge.client.ClientRegistry;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import java.awt.event.KeyEvent;
 
 
 @Mod("spectatorquickswitch")
-public class SpectatorQuickSwitch
-{
+public class SpectatorQuickSwitch {
     public static final KeyMapping spectatorKey = new KeyMapping("spectatorquickswitch.toggle_spectator", KeyEvent.VK_G, "spectatorquickswitch.spectator_category");
 
-    public SpectatorQuickSwitch()
-    {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::initClient);
+    public SpectatorQuickSwitch() {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void initClient(final FMLClientSetupEvent event) {
-        ClientRegistry.registerKeyBinding(spectatorKey);
-    }
-
     @SubscribeEvent
-    public void onKeyInput(InputEvent.KeyInputEvent event) {
+    public void onKeyInput(InputEvent.Key event) {
         if (SpectatorQuickSwitch.spectatorKey.isDown()) {
             SpectatorSwitcher.switchGameMode();
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = "keepcommandhistory", value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void registerKeys(RegisterKeyMappingsEvent event) {
+            event.register(spectatorKey);
         }
     }
 }
